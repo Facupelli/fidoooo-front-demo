1c6ab2bf2bde604c45386f9311445cecd144dd0c
Facundo Personal <facundopellicer4@gmail.com>
2023-12-17 23:48:13 -0300

    replace service accoutn json

--- src/lib/firebase-admin-config.ts
+++ src/lib/firebase-admin-config.ts
@@ -1,12 +1,12 @@
 import { initializeApp, getApps, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
-import serviceAccount from "../../service-account.json";
+// import serviceAccount from "../../service-account.json";
 
 const firebaseAdminConfig = {
   credential: cert({
-    clientEmail: serviceAccount.client_email,
-    privateKey: serviceAccount.private_key,
-    projectId: serviceAccount.project_id,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    projectId: process.env.FIREBASE_PROJECT_ID,
   }),
 };
 
